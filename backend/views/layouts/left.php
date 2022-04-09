a5d4d7e27d6bdf0d848c5adab07f477b1a1c1023
Eridiant <eridiante@gmail.com>
2022-04-09 20:02:28 +0300

    fix layout style

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -2,18 +2,6 @@
 
     <section class="sidebar">
 
-        <!-- Sidebar user panel -->
-        <div class="user-panel">
-            <div class="pull-left image">
-                <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
-            </div>
-            <div class="pull-left info">
-                <p>Alexander Pierce</p>
-
-                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-            </div>
-        </div>
-
         <?= dmstr\widgets\Menu::widget(
             [
                 'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
