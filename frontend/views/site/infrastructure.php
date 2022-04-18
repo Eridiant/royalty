5c40db39f8c768010c7f63ff14ba4d5cb8c98485
Eridiant <eridiante@gmail.com>
2022-04-18 12:46:04 +0300

    f add infr img+text

--- frontend/views/site/infrastructure.php
+++ frontend/views/site/infrastructure.php
@@ -1,23 +1,27 @@
 <div class="infrastructure">
+    <div class="container" style="max-width: 1600px; margin-left: auto; margin-right: auto">
+        <h2 class="title">Инфраструктура</h2>
+    </div>
+    <div></div>
     <div class="container-lg" style="max-width: 1920px; margin-left: auto; margin-right: auto">
         <div class="pattern"></div>
         <div class="container" style="max-width: 1600px; margin-left: auto; margin-right: auto">
             <div class="infrastructure-item">
                 <picture>
-                    <img src="/images/infrastructure/infr-1.jpg">
+                    <img src="/images/infrastructure/infrastructure/infr-1.jpg">
                 </picture>
-                <p class="title small">Открытый и закрытый бассейн</p>
+                <p class="title small">Эксплуатируемая крыша</p>
                 <p class="infrastructure-text">
-                    “Royal development” - это премиальный жилой комплекс на 1-й береговой линии, в 150 метрах от моря. В продаже  апартаменты комфорт и бизнес класса от 28 кв.метров с видом на море и горы.
+                    Уникальность нашему проекту придает многофункциональная оборудованная крыша на крыше будет несколько зон: зона отдыха, сезонный бассейн и кафе, кино площадка, озеленение со сквером. Жители комплекса смогут насладиться комплексным досугом не выходя из дома.
                 </p>
             </div>
             <div class="infrastructure-item">
                 <picture>
-                    <img src="/images/infrastructure/infr-2.jpg">
+                    <img src="/images/infrastructure/infrastructure/infr-2.jpg">
                 </picture>
-                <p class="title small">Фитнес зал</p>
+                <p class="title small">Паркинг</p>
                 <p class="infrastructure-text">
-                    “Royal development” - это премиальный жилой комплекс на 1-й береговой линии, в 150 метрах от моря. В продаже  апартаменты комфорт и бизнес класса от 28 кв.метров с видом на море и горы.
+                    Зоны парковок имеют уровни, бесплатную парковка для гостей,  подземный паркинг для жителей комплекса . Парковка имеет несколько систем вентиляции которые позволяют защитить ее от влажности, также парковка имеет места для людей с ограниченными возможностями.
                 </p>
             </div>
         </div>
@@ -27,11 +31,11 @@
         <div class="container" style="max-width: 1600px; margin-left: auto; margin-right: auto">
             <div class="infrastructure-large">
                 <picture>
-                    <img src="/images/infrastructure/infr-3.jpg">
+                    <img src="/images/infrastructure/infrastructure/infr-3.jpg">
                 </picture>
-                <p class="title small">Собственный  пляж , услуги гольф-машин</p>
+                <p class="title small">Детская игровая площадка</p>
                 <p class="infrastructure-text">
-                    “Royal development” - это премиальный жилой комплекс на 1-й береговой линии, в 150 метрах от моря. В продаже  апартаменты комфорт и бизнес класса от 28 кв.метров с видом на море и горы.
+                    Роял девелопмент ценит досуг для вас и  ваших детей, специально для маленьких жильцов будет построен сквер с детской площадкой зона 120 кв
                 </p>
             </div>
         </div>
@@ -41,66 +45,98 @@
         <div class="container" style="max-width: 1600px; margin-left: auto; margin-right: auto">
             <div class="infrastructure-item">
                 <picture>
-                    <img src="/images/infrastructure/infr-4.jpg">
+                    <img src="/images/infrastructure/infrastructure/infr-4.jpg">
                 </picture>
-                <p class="title small">Coworking зона</p>
+                <p class="title small">Сквер</p>
                 <p class="infrastructure-text">
-                    “Royal development” - это премиальный жилой комплекс на 1-й береговой линии, в 150 метрах от моря. В продаже  апартаменты комфорт и бизнес класса от 28 кв.метров с видом на море и горы.
+                    Игровая площадка будет иметь отдельную зону для отдыха на улице
                 </p>
             </div>
             <div class="infrastructure-item">
                 <picture>
-                    <img src="/images/infrastructure/infr-5.jpg">
+                    <img src="/images/infrastructure/infrastructure/infr-5.jpg">
                 </picture>
-                <p class="title small">Конференц-зал</p>
+                <p class="title small">Открытая спортивная площадка</p>
                 <p class="infrastructure-text">
-                    “Royal development” - это премиальный жилой комплекс на 1-й береговой линии, в 150 метрах от моря. В продаже  апартаменты комфорт и бизнес класса от 28 кв.метров с видом на море и горы.
+                    Для любителей спорта на улице вас будет ждать оборудования открытая площадка для занятий спортом, мини футбол, стритбол, кроссфит итд
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-lg" style="max-width: 1920px; margin-left: auto; margin-right: auto">
+        <div class="pattern right"></div>
+        <div class="container" style="max-width: 1600px; margin-left: auto; margin-right: auto">
+            <div class="infrastructure-large">
+                <picture>
+                    <img src="/images/infrastructure/infrastructure/infr-6.jpg">
+                </picture>
+                <p class="title small">Фитнес центр</p>
+                <p class="infrastructure-text">
+                    Будет располагаться на 2-м этаже, для фитнеса выделено свыше 200 кв.метров, поддержка здорового образа жизни является для нас приятным дополнением концепции нашего комплекса
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="container-lg" style="max-width: 1920px; margin-left: auto; margin-right: auto">
+        <div class="pattern"></div>
+        <div class="container" style="max-width: 1600px; margin-left: auto; margin-right: auto">
+            <div class="infrastructure-item">
+                <picture>
+                    <img src="/images/infrastructure/infrastructure/infr-7.jpg">
+                </picture>
+                <p class="title small">Открытый Бассейн</p>
+                <p class="infrastructure-text">
+                    Будет работать в теплый сезон, вы сможете насладиться морем в собственном бассейне на высоте 50 метров.
+                </p>
+            </div>
+            <div class="infrastructure-item">
+                <picture>
+                    <img src="/images/infrastructure/infrastructure/infr-8.jpg">
+                </picture>
+                <p class="title small">Отель</p>
+                <p class="infrastructure-text">
+                    В развитии первых 2 лет наш комплекс планирует открытие бутикового отеля для развития комплекса и его ценности для клиентов
                 </p>
             </div>
         </div>
     </div>
     <div class="container" style="max-width: 1600px; margin-left: auto; margin-right: auto">
-        <div class="infrastructure-desc">
-            <h2 class="title">Инфраструктура</h2>
-            <div class="items item">
-                <p>Эксплуатируемая крыша</p>
-                <p>
-                    Уникальность нашему проекту придает многофункциональная оборудованная крыша на крыше будет несколько зон: зона отдыха, сезонный бассейн и кафе, кино площадка, озеленение со сквером. Жители комплекса смогут насладиться комплексным досугом не выходя из дома.
-                </p>
-                <p>
-                    Паркинг - зоны парковок имеют уровни, бесплатную парковка для гостей,  подземный паркинг для жителей комплекса . Парковка имеет несколько систем вентиляции которые позволяют защитить ее от влажности, также парковка имеет места для людей с ограниченными возможностями.
-                </p>
-                <p>
-                    Детская игровая площадка- Роял девелопмент ценит досуг для вас и  ваших детей, специально для маленьких жильцов будет построен сквер с детской площадкой зона 120 кв. 
-                </p>
-                <p>
-                    Сквер- игровая площадка будет иметь отдельную зону для отдыха на улице.
-                </p>
-                <p>
-                    Открытая спортивная площадка- для любителей спорта на улице вас будет ждать оборудования открытая площадка для занятий спортом, мини футбол, стритбол, кроссфит итд 
-                </p>
-                <p>
-                    Фитнес центр- будет располагаться на 2-м этаже, для фитнеса выделено свыше 200 кв.метров, поддержка здорового образа жизни является для нас приятным дополнением концепции нашего комплекса.
-                </p>
-                <p>
-                    Открытый Бассейн - будет работать в теплый сезон, вы сможете насладиться морем в собственном бассейне на высоте 50 метров.
-                </p>
-                <p>
-                    Отель - в развитии первых 2 лет наш комплекс планирует открытие бутикового отеля для развития комплекса и его ценности для клиентов.
+        <h2 class="title">Сервисы</h2>
+    </div>
+    <div class="container-lg" style="max-width: 1920px; margin-left: auto; margin-right: auto">
+        <div class="pattern"></div>
+        <div class="container" style="max-width: 1600px; margin-left: auto; margin-right: auto">
+            <div class="infrastructure-item">
+                <picture>
+                    <img src="/images/infrastructure/location/location-1.jpg">
+                </picture>
+                <p class="title small">Ресепшн 24\7</p>
+                <p class="infrastructure-text">
+                    Для жильцов будет работать стойка рецепции на 1 этаже, в круглосуточном режиме. Вы всегда сможете получить информацию, вам ответят на все вопросы и окажут помощь в любой момент
                 </p>
             </div>
-            <div class="items item">
-                <h2 class="title">Сервисы</h2>
-                <p>Ресепшн 24\7</p>
-                <p>
-                    Для жильцов будет работать стойка рецепции на 1 этаже, в круглосуточном режиме. Вы всегда сможете получить информацию, вам ответят на все вопросы и окажут помощь.
+            <div class="infrastructure-item">
+                <picture>
+                    <img src="/images/infrastructure/location/location-2.jpg">
+                </picture>
+                <p class="title small">Охрана и видеонаблюдение 24\7</p>
+                <p class="infrastructure-text">
+                    Безопасность в нашем комплексе оказывает специализированное агентство в режиме 24/7, сохранность вас и ваших вещей под постоянной защитой, поддержку оказывает  системы видеонаблюдения
                 </p>
-                <p>Охрана и видеонаблюдение 24\7</p>
-                <p>
-                    Безопасность в нашем комплексе оказывает специализированное агентство в режиме 24/7, сохранность вас и ваших вещей под постоянной защитой, поддержку оказывает  системы видеонаблюдения.
-                </p>
-                <p>
-                    Клининг- управляющая компания роял девелопмент, очень внимательно относиться к чистоте и экологии, наш комплекс ежедневно проводит сухую и влажную уборку, антибактериальную уборку. Для наших жильцов мы предлагаем услуги по уборке ваших квартир.
+            </div>
+        </div>
+    </div>
+    <div class="container-lg" style="max-width: 1920px; margin-left: auto; margin-right: auto">
+        <div class="pattern right"></div>
+        <div class="container" style="max-width: 1600px; margin-left: auto; margin-right: auto">
+            <div class="infrastructure-large">
+                <picture>
+                    <img src="/images/infrastructure/location/location-3.jpg">
+                </picture>
+                <p class="title small">Клининг</p>
+                <p class="infrastructure-text">
+                    Управляющая компания роял девелопмент, очень внимательно относиться к чистоте и экологии, наш комплекс ежедневно проводит сухую и влажную уборку, антибактериальную уборку. Для наших жильцов мы предлагаем услуги по уборке ваших квартир
                 </p>
             </div>
         </div>
