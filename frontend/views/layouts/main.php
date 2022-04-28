9590ed733475ad5d00e74677ebdc09b1a0f97296
Eridiant <eridiante@gmail.com>
2022-04-28 13:35:29 +0300

    f add fav

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -24,6 +24,18 @@ $cLang = \backend\modules\language\models\Language::find()->where(['deleted_at' 
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png">
+	<link rel="manifest" href="/images/favicon/site.webmanifest">
+	<link rel="mask-icon" href="/images/favicon/safari-pinned-tab.svg" color="#5bbad5">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="theme-color" content="#ffffff">
+
+    <?php $this->registerMetaTag(['property' => 'og:title', 'content' => 'Premium class high rise residential complex']); ?>
+    <?php $this->registerMetaTag(['property' => 'og:image', 'content' => 'https://calligraphy-batumi.com/images/image.jpg']); ?>
+
     <?php $this->registerCsrfMetaTags() ?>
     <?php
     if (!Yii::$app->user->isGuest) {
